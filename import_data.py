cb62fe83b6856e7a3a8cac75f34d25b23a8920c2
jannettim <jannettim@gmail.com>
2017-03-26 15:23:50 -0400

    Can import all files. Files aren't clean. Need to check for errors

--- import_data.py
+++ import_data.py
@@ -6,48 +6,79 @@ import os
 import re
 import sys
 from collections import Counter
+from progress.bar import Bar
 
-f_list = os.listdir("data\\harvard_dataverse")
-full_df = None
-count = 0
-for f in f_list:
+def concat_sets(filename=None):
+    f_list = os.listdir("data\\harvard_dataverse")
+    full_df = None
+    count = 0
 
-    print(f)
+    bar = Bar('Processing', max=len(f_list))
+
+    for f in f_list:
+
+        # print(f)
+
+        columns = []
+
+        if count >= 20:
+            break
+
+        if re.search(r".+\.tab$", f):
+
+            df = pandas.DataFrame.from_csv(os.path.join("data\\harvard_dataverse", f), header=0, sep="\t")
+            for col in df.columns.values:
+                if re.search(r"(.*STH.*)|(.*STS.*)|(.*USP.*)|(.*GOV.*)", col):
+                    new_col = re.sub(r"(?<=g|s|p|r)\d+(?!$)", "", col)
+                    df.rename(columns={col: new_col}, inplace=True)
+                    columns.append(new_col)
+                elif re.search(r"(?<!_)precinct|precinct_code|state|year|county|parish", col):
+                    if col == "parish":
+                        df.rename(columns={"parish": "county"}, inplace=True)
+                        col = "county"
+                    columns.append(col)
+                else:
+                    pass
+
+            if count == 0:
+                full_df = df[columns]
+                count += 1
+            else:
+                full_df = pandas.concat([full_df, df[columns]])
+                # count += 1
+
+        bar.next()
+
+    bar.finish()
+    full_df.to_csv("data\\heda_concatenated.txt", sep="\t")
+    return full_df
+
+def test_set(filename):
+    df = pandas.DataFrame.from_csv(filename, header=0, sep="\t")
 
     columns = []
-    # full_df = None
 
-    # if not f.startswith("AK"):
-    #     count = 0
-    #     break
+    print(df.columns.values)
 
-    if count >= 20:
-        break
-
-    if re.search(r".+\.tab$", f):
-
-        df = pandas.DataFrame.from_csv(os.path.join("data\\harvard_dataverse", f), header=0, sep="\t")
-        for col in df.columns.values:
-            if re.search(r"(.*STH.*)|(.*STS.*)|(.*USP.*)|(.*GOV.*)", col):
-                new_col = re.sub(r"(?<=\w)\d+(?!$)", "", col)
-                df.rename(columns={col: new_col}, inplace=True)
-                columns.append(new_col)
-            elif re.search(r"(?<!_)precinct|precinct_code|state|year|county|parish", col):
-                if col == "parish":
-                    df.rename(columns={"parish": "county"}, inplace=True)
-                columns.append(col)
-            else:
-                pass
-
-        if count == 0:
-            full_df = df[columns]
-            count += 1
+    for col in df.columns.values:
+        if re.search(r"(.*STH.*)|(.*STS.*)|(.*USP.*)|(.*GOV.*)", col):
+            new_col = re.sub(r"(?<=g|s|p|r)\d+(?!$)", "", col)
+            df.rename(columns={col: new_col}, inplace=True)
+            columns.append(new_col)
+        elif re.search(r"(?<!_)precinct|precinct_code|state|year|county|parish", col):
+            if col == "parish":
+                df.rename(columns={"parish": "county"}, inplace=True)
+                col = "county"
+            columns.append(col)
         else:
+            pass
 
-            full_df = pandas.concat([full_df, df[columns]])
-            count += 1
+    print(columns)
 
+    # for c in columns:
+    #     print(c)
+    #     print(df[c].dtype)
+    # print(df["g_STH_dv2"])
 
-app = QtGui.QApplication(sys.argv)
-datatable = DataTable(full_df, editable=False, window_title="Whole Dataset")
-app.exec()
+# test_set("data\\harvard_dataverse\\LA_2000.tab")
+concat_sets()

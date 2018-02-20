048ec40a2a196d386b5e68a2ffc53a2b2a160da5
jannettim <jannettim@gmail.com>
2018-02-20 16:08:27 -0500

    Finished efficieny gap calculation. Removed old modules

--- analysis.py
+++ analysis.py
@@ -7,51 +7,104 @@ cnxn = sqlite3.connect("house_elections.db")
 cn = cnxn.cursor()
 
 
-def get_winner():
+def get_winner(election_type):
 
-    cn.execute("SELECT e.election_id, e.state, e.year, r.votes, r.party FROM elections e "
-               "LEFT OUTER JOIN results r ON r.election_id = e.election_id "
-               "INNER JOIN ("
-                        "SELECT e2.election_id, MAX(r2.votes) AS votes FROM elections e2 "
-                        "LEFT OUTER JOIN results r2 ON r2.election_id = e2.election_id "
-                        "GROUP BY e2.election_id) b ON r.votes = b.votes AND b.election_id=e.election_id")
+    if election_type:
 
-    return cn.fetchall()
+        cn.execute("SELECT e.election_id, e.state, e.year, r.votes, r.party FROM elections e "
+                   "LEFT OUTER JOIN results r ON r.election_id = e.election_id "
+                   "INNER JOIN (SELECT r2.election_id, MAX(r2.votes) as Votes FROM results r2 "
+                   "GROUP BY r2.election_id) b ON r.Election_Id=b.election_id AND b.votes=r.votes "
+                   "WHERE r.party IN ('Democrat', 'Republican') AND e.Federal=?", (election_type,))
 
+    else:
 
-def get_loser():
-
-    cn.execute("SELECT e.election_id, r.votes, r.party FROM elections e "
-               "LEFT OUTER JOIN results r ON r.election_id = e.election_id "
-               "INNER JOIN ("
-                            "SELECT e2.election_id, MIN(r2.votes) AS votes FROM elections e2 "
-                            "LEFT OUTER JOIN results r2 ON r2.election_id = e2.election_id "
-                            "GROUP BY e2.election_id) b ON r.votes = b.votes AND b.election_id=e.election_id")
+        cn.execute("SELECT e.election_id, e.state, e.year, r.votes, r.party FROM elections e "
+                   "LEFT OUTER JOIN results r ON r.election_id = e.election_id "
+                   "INNER JOIN (SELECT r2.election_id, MAX(r2.votes) as Votes FROM results r2 "
+                   "GROUP BY r2.election_id) b ON r.Election_Id=b.election_id AND b.votes=r.votes "
+                   "WHERE r.party IN ('Democrat', 'Republican')")
 
     return cn.fetchall()
 
 
-def compare_wl(winners, losers):
+def get_losers(election_type):
+
+    if election_type:
+
+        cn.execute("SELECT e.election_id, r.votes, r.party FROM elections e "
+                   "LEFT OUTER JOIN results r ON r.election_id = e.election_id "
+                   "INNER JOIN (SELECT r2.election_id, MAX(r2.votes) as Votes FROM results r2 "
+                   "GROUP BY r2.election_id) b ON r.Election_Id=b.election_id AND b.votes<>r.votes "
+                   "WHERE r.party IN ('Democrat', 'Republican') AND e.Federal=? AND r.votes > 0", (election_type,))
+
+    else:
+        cn.execute("SELECT e.election_id, r.votes, r.party FROM elections e "
+                   "LEFT OUTER JOIN results r ON r.election_id = e.election_id "
+                   "INNER JOIN (SELECT r2.election_id, MAX(r2.votes) as Votes FROM results r2 "
+                   "GROUP BY r2.election_id) b ON r.Election_Id=b.election_id AND b.votes<>r.votes "
+                   "WHERE r.party IN ('Democrat', 'Republican')  AND r.votes > 0")
+
+    return cn.fetchall()
 
 
-    win_df = pd.DataFrame(winners, columns=["Election_id", "State", "Year", "Votes", "Party"])
-    loss_df = pd.DataFrame(losers, columns=["Election_id", "Votes", "Party"])
+def get_total_votes(election_type):
 
-    results_df = win_df.merge(loss_df, on=["Election_id"], suffixes=("_win", "_loss"), how="outer")
-    # results_df = results_df.loc[results_df["Votes_loss"] != 0]
+    if election_type:
+
+        cn.execute("SELECT e.election_id, SUM(r.votes) "
+                   "FROM elections e "
+                   "INNER JOIN results r ON r.election_id=e.election_id "
+                   "WHERE e.Federal=? "
+                   "GROUP BY e.election_id", (election_type,))
+
+    else:
+
+        cn.execute("SELECT e.election_id, SUM(r.votes) "
+                   "FROM elections e "
+                   "INNER JOIN results r ON r.election_id=e.election_id "
+                   "GROUP BY e.election_id")
+
+    return cn.fetchall()
+
+
+def compare_wl(election_type):
+    win_df = pd.DataFrame(get_winner(election_type), columns=["Election_id", "State", "Year", "Votes", "Party"])
+    loss_df = pd.DataFrame(get_losers(election_type), columns=["Election_id", "Votes", "Party"])
+    total_votes = pd.DataFrame(get_total_votes(election_type), columns=["Election_id", "TotalVotes"])
+
+    results_df = win_df.merge(loss_df, on=["Election_id"], suffixes=("_win", "_loss"), how="inner")
+    results_df = results_df.merge(total_votes, on=["Election_id"], how="inner")
 
     return results_df
 
 
-def get_efficiency_gap(win, loss):
+def get_efficiency_gap(election_type):
 
-    results = compare_wl(win, loss)
+    if election_type.lower() not in ("federal", "state", "all"):
 
-    results["wasted_wins"] = results["Votes_win"] - (((results["Votes_win"] + results["Votes_loss"])*.5) + 1).apply(math.floor)
+        raise TypeError("Type is not correctly supplied.")
+
+    if election_type.lower() == "federal":
+
+        fed_bool = 1
+
+    elif election_type.lower() == "all":
+
+        fed_bool = 1
+
+    else:
+
+        fed_bool = None
+
+    results = compare_wl(fed_bool)
+
+    results["wasted_wins"] = results["Votes_win"] - (((results["TotalVotes"]) * .5) + 1).apply(
+        math.floor)
     results = results.loc[results["Votes_loss"] != 0]
     results["total_votes"] = results["Votes_win"] + results["Votes_loss"]
 
-    total_votes = results[["State", "Year", "total_votes"]].groupby(["State", "Year"], as_index=False).sum()
+    #total_votes = results[["State", "Year", "total_votes"]].groupby(["State", "Year"], as_index=False).sum()
 
     wasted_votes_loss = results.groupby(["State", "Year", "Party_loss"], as_index=False)["Votes_loss"].sum()
     wasted_votes_wins = results.groupby(["State", "Year", "Party_win"], as_index=False)["wasted_wins"].sum()
@@ -75,16 +128,35 @@ def get_efficiency_gap(win, loss):
                             wasted_df_rep[["State", "Year", "wasted_votes"]],
                             on=["State", "Year"], suffixes=["_dem", "_rep"])
 
+    total_votes = results.groupby(["State", "Year"], as_index=False)["TotalVotes"].sum()
+
     efficency_df = pd.merge(efficency_df, total_votes, on=["State", "Year"])
-    efficency_df["gap"] = (efficency_df["wasted_votes_dem"] - efficency_df["wasted_votes_rep"])/efficency_df["total_votes"]
+    efficency_df["gap"] = (efficency_df["wasted_votes_dem"] - efficency_df["wasted_votes_rep"]) / efficency_df[
+        "TotalVotes"]
+
+    efficency_df = efficency_df.loc[(efficency_df.Year >= 2000) & (efficency_df.Year <= 2010)]
+
+    efficency_df = efficency_df.groupby(["State"], as_index=False)["gap"].mean()
 
     return efficency_df
 
 
-if __name__ == '__main__':
+def simulate_elections(win, loss):
+    cn.execute("SELECT e.election_id, e.state, e.year, r.votes, r.party FROM elections e "
+               "LEFT OUTER JOIN results r ON r.election_id = e.election_id")
+    print(cn.fetchall())
+    # print(compare_wl(win, loss))
 
-    win = get_winner()
-    loss = get_loser()
-    efficency_df = get_efficiency_gap(win, loss)
+
+if __name__ == '__main__':
+    # print(get_dems())
+    # print(get_rep())
+    # print(get_other())
+    # win = get_winner()
+    # print(win)
+    # loss = get_loser()
+    # compare_wl()
+    efficency_df = get_efficiency_gap("state")
     efficency_df.to_csv("test.csv")
     os.system("xdg-open test.csv")
+    # simulate_elections(win, loss)
--- import_data.py
+++ import_data.py
@@ -1,85 +1,260 @@
-import pandas
-from PyQt4 import QtCore, QtGui
-from DataTables import DataTable
-import sys
+import pandas as pd
 import os
 import re
-import sys
-from collections import Counter
-from progress.bar import Bar
+import sqlite3
+from numpy import nan
+from lxml import etree
+import requests
 
-def concat_sets(election_code, filename=None):
-    f_list = os.listdir("data\\harvard_dataverse")
-    full_df = None
-    count = 0
 
-    bar = Bar('Processing', max=len(f_list))
+def create_database(db_path):
 
-    for f in f_list:
+    cnxn = sqlite3.connect(db_path)
+    cn = cnxn.cursor()
 
-        # print(f)
+    cn.execute("DROP TABLE IF EXISTS elections")
+    cn.execute("DROP TABLE IF EXISTS results")
+    cnxn.commit()
 
-        columns = []
+    cn.execute("CREATE TABLE elections (Election_Id INTEGER PRIMARY KEY NOT NULL, "
+               "State TEXT NOT NULL, District VARCHAR2(30) NOT NULL, Year INTEGER NOT NULL, Federal INTEGER NOT NULL "
+               "CHECK(Federal in (0, 1)))")
 
-        if count >= 20:
-            break
+    cn.execute("CREATE TABLE results (Election_Id INTEGER NOT NULL, Party TEXT NOT NULL, Votes INTEGER NOT NULL, "
+               "Incumbent INTEGER, "
+               "CHECK(Incumbent in (0, 1) OR Incumbent IS NULL), "
+               "PRIMARY KEY(Election_Id, Party), "
+               "FOREIGN KEY(Election_Id) REFERENCES elections(Election_Id)) ")
 
-        if re.search(r".+\.tab$", f):
+    cnxn.commit()
 
-            df = pandas.DataFrame.from_csv(os.path.join("data\\harvard_dataverse", f), header=0, sep="\t")
-            for col in df.columns.values:
-                if re.search(election_code, col):
-                    new_col = re.sub(r"(?<=g|s|p|r)\d+(?!$)", "", col)
-                    df.rename(columns={col: new_col}, inplace=True)
-                    columns.append(new_col)
-                elif re.search(r"(?<!_)precinct|precinct_code|state|year|county|parish", col):
-                    if col == "parish":
-                        df.rename(columns={"parish": "county"}, inplace=True)
-                        col = "county"
-                    columns.append(col)
-                else:
-                    pass
+    cn.close()
+    cnxn.close()
 
-            if count == 0:
-                full_df = df[columns]
-                count += 1
-            else:
-                full_df = pandas.concat([full_df, df[columns]])
-                # count += 1
 
-        bar.next()
+def import_ispsr(directory):
 
-    bar.finish()
-    full_df.to_csv(filename, sep="\t")
-    return full_df
+    dir_files = [d for d in os.listdir(directory) if re.match(r"DS\d{1,4}", d) and d != "DS0049"]
 
-def test_set(filename):
-    df = pandas.DataFrame.from_csv(filename, header=0, sep="\t")
+    df = pd.DataFrame()
 
-    columns = []
+    year_lookup = pd.read_excel("data/ICPSR_06311/FolderCode_Year_Lookup.xlsx", "Sheet1")
+    year_lookup["Code"] = year_lookup["Code"].astype(str).str.zfill(4)
 
-    print(df.columns.values)
+    state_lookup = pd.read_excel("data/ICPSR_06311/Code_StateLookup.xlsx", "Sheet1")
 
-    for col in df.columns.values:
-        if re.search(r"(.*STH.*)|(.*STS.*)|(.*USH.*)|(.*USS.*)|(.*USP.*)|(.*GOV.*)", col):
-            new_col = re.sub(r"(?<=g|s|p|r)\d+(?!$)", "", col)
-            df.rename(columns={col: new_col}, inplace=True)
-            columns.append(new_col)
-        elif re.search(r"(?<!_)precinct|precinct_code|state|year|county|parish", col):
-            if col == "parish":
-                df.rename(columns={"parish": "county"}, inplace=True)
-                col = "county"
-            columns.append(col)
-        else:
-            pass
+    for d in dir_files:
 
-    print(columns)
+        year = year_lookup.loc[year_lookup.Text.str.cat(year_lookup.Code) == d]["Year"].values[0]
 
-    # for c in columns:
-    #     print(c)
-    #     print(df[c].dtype)
-    # print(df["g_STH_dv2"])
+        df2 = pd.read_csv(os.path.join(directory, d, os.listdir(os.path.join(directory, d))[0]), sep="\s+",
+                         names=["State_Code", "District", "Incumbent", "Republican Votes", "Democratic Votes"])
+        df2["Year"] = year
 
-# test_set("data\\harvard_dataverse\\LA_2000.tab")
-# (.*STH.*)|(.*STS.*)|(.*USH.*)|(.*USS.*)|(.*USP.*)|(.*GOV.*)
-concat_sets(r".*STH_\wv.*", filename="data\\sth_concatenated.txt")
+        df = df.append(df2)
+
+    df = pd.merge(df, state_lookup, left_on="State_Code", right_on="Code", how="left")
+
+    df.reset_index(inplace=True)
+
+    df.rename(columns={"index": "Election_Id"}, inplace=True)
+
+    df["Election_Id"] = df["Election_Id"] + 1
+
+    elections = df[["Election_Id", "State", "District", "Year"]]
+
+    dem_votes = df[["Election_Id", "Democratic Votes"]]
+    dem_votes["Incumbent"] = 0
+    dem_votes.loc[df["Election_Id"].isin(df.loc[((df["Incumbent"] == 1) | (df["Incumbent"] == 3))]["Election_Id"].tolist()), "Incumbent"] = 1
+    dem_votes.rename(columns={"Democratic Votes": "Votes"}, inplace=True)
+    dem_votes["Party"] = "Democrat"
+
+    repub_votes = df[["Election_Id", "Republican Votes"]]
+    repub_votes["Incumbent"] = 0
+    repub_votes.loc[df["Election_Id"].isin(df.loc[((df["Incumbent"] == -1) | (df["Incumbent"] == 3))]["Election_Id"].tolist()), "Incumbent"] = 1
+    repub_votes.rename(columns={"Republican Votes": "Votes"}, inplace=True)
+    repub_votes["Party"] = "Republican"
+
+    # print(dem_votes)
+    # print(repub_votes)
+
+    results = dem_votes.append(repub_votes)
+
+    results.sort_values("Election_Id", inplace=True)
+    results = results.loc[~results["Election_Id"].isin(results.loc[results["Votes"] == -9]["Election_Id"])]
+
+    elections = elections.loc[~elections["Election_Id"].isin(results.loc[results["Votes"] == -9]["Election_Id"])]
+
+    # print(os.path.join(directory, "DS0049", os.listdir(os.path.join(directory, "DS0049"))[0]))
+    # exceptions = pd.read_csv(os.path.join(directory, "DS0049", os.listdir(os.path.join(directory, "DS0049"))[0]),
+    #                          sep="\s+", skiprows=1,
+    #                          names=["Year", "State", "District", "Democratic", "Republican", "Minor", "Total Votes"])
+    # print(exceptions)
+    #
+    return elections, results
+
+
+def clean_cq_data(datafile, sheet):
+
+    df = pd.read_excel(datafile, sheet, skiprows=4, names=["State_Abbr", "District", "Dem_Can", "Dem_Votes",
+                                                           "Dem_Per", "Rep_Can", "Rep_Votes", "Rep_Per",
+                                                           "Highest_Minor", "Minor_Votes", "Minor %", "Year"],
+                       skip_footer=2)
+    df.loc[df.District == "At Large", "District"] = 98
+
+    state_name = pd.read_excel("data/StateAbbr_Name_Lookup.xlsx", "Sheet1")
+    df = pd.merge(df, state_name, how="left", on=["State_Abbr"])
+
+    df.reset_index(inplace=True)
+    df.rename(columns={"index": "Election_Id"}, inplace=True)
+
+    df["Election_Id"] = df["Election_Id"] + 1
+
+    elections = df[["Election_Id", "State", "District", "Year"]]
+
+    # dem_votes = df[["Election_Id", "Dem_Votes"]]
+    # dem_votes["Incumbent"] = 0
+    # dem_votes.loc[df["Election_Id"].isin(
+    #     df.loc[((df["Incumbent"] == 1) | (df["Incumbent"] == 3))]["Election_Id"].tolist()), "Incumbent"] = 1
+    # dem_votes.rename(columns={"Democratic Votes": "Votes"}, inplace=True)
+    # dem_votes["Party"] = "Democrat"
+    #
+    # repub_votes = df[["Election_Id", "Republican Votes"]]
+    # repub_votes["Incumbent"] = 0
+    # repub_votes.loc[df["Election_Id"].isin(
+    #     df.loc[((df["Incumbent"] == -1) | (df["Incumbent"] == 3))]["Election_Id"].tolist()), "Incumbent"] = 1
+    # repub_votes.rename(columns={"Republican Votes": "Votes"}, inplace=True)
+    # repub_votes["Party"] = "Republican"
+
+    # print(dem_votes)
+    # print(repub_votes)
+
+    # results = dem_votes.append(repub_votes)
+
+    # results.sort_values("Election_Id", inplace=True)
+    #
+    # print(df)
+    #
+    # 1/0
+
+    # print(df.Dem_Votes + df.Rep_Votes + df.Minor_Votes)
+    df["Total_Votes"] = df[["Dem_Votes", "Rep_Votes", "Minor_Votes"]].sum(axis=1)
+    df["Losing Wasted Votes"] = df["Total_Votes"] - df[["Dem_Votes", "Rep_Votes", "Minor_Votes"]].max(axis=1)
+
+    df["Winning Wasted Votes"] = df[["Dem_Votes", "Rep_Votes", "Minor_Votes"]].max(axis=1) + df["Total_Votes"]/2
+
+    print(df[["Losing Wasted Votes", "Winning Wasted Votes"]])
+
+
+def import_princeton(datafile):
+
+    df = pd.read_csv(datafile)
+
+    df["Republican Votes"] = df["Republican Votes"].str.replace(",", "")
+    df["Democratic Votes"] = df["Democratic Votes"].str.replace(",", "")
+
+    df.reset_index(inplace=True)
+
+    df.rename(columns={"index": "Election_Id"}, inplace=True)
+
+    df["Election_Id"] += 1
+
+    elections = df[["Election_Id", "State", "District", "Year"]]
+    elections["Federal"] = 1
+
+    dem_votes = df[["Election_Id", "Democratic Votes"]]
+    dem_votes["Party"] = "Democrat"
+    dem_votes.rename(columns={"Democratic Votes": "Votes"}, inplace=True)
+
+    rep_votes = df[["Election_Id", "Republican Votes"]]
+    rep_votes["Party"] = "Republican"
+    rep_votes.rename(columns={"Republican Votes": "Votes"}, inplace=True)
+
+    results = dem_votes.append(rep_votes)
+    results["Incumbent"] = nan
+    results.sort_values(by="Election_Id", inplace=True)
+
+    return elections, results
+
+
+def import_princeton_state(datafiles):
+
+    cnxn = sqlite3.connect("house_elections.db")
+    cn = cnxn.cursor()
+
+    if isinstance(datafiles, (list,tuple)):
+
+        df = pd.DataFrame()
+
+        for d in datafiles:
+
+            df = df.append(pd.read_csv(d))
+
+    else:
+
+        df = pd.read_csv(datafiles)
+
+    df.reset_index(df, inplace=True)
+    df.drop("index", axis=1, inplace=True)
+
+    cn.execute("SELECT MAX(Election_Id) FROM Elections")
+
+    df["Election_Id"] = df.index + 1 + cn.fetchone()[0]
+
+    df.rename(columns={"State": "State_Abbr"}, inplace=True)
+    state_name = pd.read_excel("data/StateAbbr_Name_Lookup.xlsx", "Sheet1")
+    df = pd.merge(df, state_name, how="left", on=["State_Abbr"])
+
+    elections = df[["Election_Id", "State", "District", "Year"]]
+
+    elections["Federal"] = 0
+
+    dem_votes = df[["Election_Id", "Dem Votes", "Incumbent"]]
+    dem_votes["Party"] = "Democrat"
+    dem_votes.rename(columns={"Dem Votes": "Votes"}, inplace=True)
+
+    rep_votes = df[["Election_Id", "GOP Votes", "Incumbent"]]
+    rep_votes["Party"] = "Republican"
+    rep_votes.rename(columns={"GOP Votes": "Votes"}, inplace=True)
+
+    other_votes = df[["Election_Id", "Other Votes", "Incumbent"]]
+    other_votes["Party"] = "Other"
+    other_votes.rename(columns={"Other Votes": "Votes"}, inplace=True)
+
+    results = dem_votes.append(rep_votes)
+    results = results.append(other_votes)
+    results = results[["Election_Id", "Votes", "Party", "Incumbent"]]
+    results.sort_values(by="Election_Id", inplace=True)
+
+    return elections, results
+
+
+def insert_princeton(elections_df, results_df):
+
+    cnxn = sqlite3.connect("house_elections.db")
+    cn = cnxn.cursor()
+
+    cn.executemany("INSERT INTO elections (Election_Id, State, District, Year, Federal) "
+                   "VALUES (?, ?, ?, ?, ?)", elections_df.values.tolist())
+
+    cnxn.commit()
+
+    cn.executemany("INSERT INTO results (Election_Id, Votes, Party, Incumbent) "
+                   "VALUES (?, ?, ?, ?)", results_df.values.tolist())
+
+    cnxn.commit()
+
+    cn.close()
+    cnxn.close()
+
+if __name__ == "__main__":
+
+    create_database("house_elections.db")
+    # icpsr = import_ispsr("/home/matt/GitRepos/ElectionData/data/ICPSR_06311")
+    # print(icpsr)
+    # clean_cq_data(datafile="data/CQ Press/House Elections/US House Elections By Year By District.xlsx", sheet="Export")
+    elections, results = import_princeton("/home/matt/GitRepos/ElectionData/data/Princeton/election_data.csv")
+    insert_princeton(elections, results)
+    state_elections, state_results = import_princeton_state(["/home/matt/GitRepos/ElectionData/data/Princeton/assembly_cleaned_data_1972_2010.csv",
+                                                             "/home/matt/GitRepos/ElectionData/data/Princeton/assembly_cleaned_data_2011_2012.csv"])
+    insert_princeton(state_elections, state_results)
